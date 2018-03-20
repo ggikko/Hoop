018459d8729b885dcc7f7bd904ffbb3e9cd3a202
ggikko <ggikko88@gmail.com>
2018-03-21 01:12:36 +0900

    make server application

---
+++ server/src/main/kotlin/me/ggikko/GgikkoApplication.kt
@@ -0,0 +1,11 @@
+package me.ggikko
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GgikkoApplication
+
+fun main(args: Array<String>) {
+    runApplication<GgikkoApplication>(*args)
+}
---
+++ server/src/test/kotlin/me/ggikko/GgikkoApplicationTests.kt
@@ -0,0 +1,16 @@
+package me.ggikko
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GgikkoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
