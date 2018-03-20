5c8f77a8f20ffff9f3e6214754ce29d19c62b0ae
ggikko <ggikko88@gmail.com>
2018-03-21 01:48:48 +0900

    start android application

---
+++ android/app/src/main/java/ggikko/me/ggikkoapplication/GgikkoApplication.kt
@@ -0,0 +1,13 @@
+package ggikko.me.ggikkoapplication
+
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+/**
+ * Created by ggikko (JiHong Park) on 2018. 3. 21..
+ */
+class GgikkoApplication : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ android/app/src/main/java/ggikko/me/ggikkoapplication/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package ggikko.me.ggikkoapplication.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ggikko.me.ggikkoapplication.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
